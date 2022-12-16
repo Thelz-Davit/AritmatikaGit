3a4727ea23b5b56186742c3c0acfdd3efce72dfa
Thelzyboi <Davitra03@gmail.com>
2022-12-16 08:30:57 +0700

    Ini adalah inisiasi Project Aritmatika

---
+++ src/aritmatikagit/Aritmatika.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aritmatikagit;
+
+/**
+ *
+ * @author Davit
+ */
+public class Aritmatika {
+    private double bilanganA;
+    private double bilanganB;
+
+    public Aritmatika(double bilanganA, double bilanganB) {
+        this.bilanganA = bilanganA;
+        this.bilanganB = bilanganB;
+    }
+
+    public Aritmatika() {
+    }
+    
+    public double getBilanganA() {
+        return bilanganA;
+    }
+
+    public void setBilanganA(double bilanganA) {
+        this.bilanganA = bilanganA;
+    }
+
+    public double getBilanganB() {
+        return bilanganB;
+    }
+
+    public void setBilanganB(double bilanganB) {
+        this.bilanganB = bilanganB;
+    }
+    
+    //Operasi Penjumlahan
+    public double Penjumlahan(){
+        return 0;
+    }
+    //Operasi Pengurangan
+    public double Pengurangan(){
+        return 0;
+    }
+    //Operasi Pembagian
+    public double Pembagian(){
+        return 0;
+    }
+    //Operasi Perkalian
+    public double Perkalian(){
+        return 0;
+    }
+}
